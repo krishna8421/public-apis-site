b9fa7f81dd738d12e20ec4a7aaf1f6f5a13f320d
Krishna Kumar <71817691+krishna8421@users.noreply.github.com>
2021-09-15 17:40:09 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import { Box, Flex,Text } from "@chakra-ui/react";
 function Footer() {
   return (
     <Box  mt='auto' mx='auto' w={["95%", "95%", "90%", "85%"]} borderTopWidth="3px" borderColor="purple.600">
-      <Flex my={10} justify="center" align="center">
+      <Flex my={8} justify="center" align="center">
         <Text fontSize="md"  >Made with ❤️ by Krishna</Text>
       </Flex>
     </Box>
