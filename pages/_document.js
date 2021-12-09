5086b6e0e705492e73230eed4e3ed893d18a345b
Krishna Kumar <71817691+krishna8421@users.noreply.github.com>
2021-12-09 13:40:18 +0530

    Fix dep

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import theme from "../styles/theme";
-import favicon from "../public/favicon.ico";
+
 export default class Document extends NextDocument {
   render() {
     return (
